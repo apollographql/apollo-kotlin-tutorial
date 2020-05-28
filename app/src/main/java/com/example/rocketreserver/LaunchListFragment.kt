17fb49e43d20068e58bfc94056f8ab68ebeb121a
Martin Bonnin <martin@mbonnin.net>
2020-05-28 16:09:38 +0200

    04-execute a request

--- app/src/main/java/com/example/rocketreserver/LaunchListFragment.kt
+++ app/src/main/java/com/example/rocketreserver/LaunchListFragment.kt
@@ -1,17 +1,44 @@
 package com.example.rocketreserver
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.apollographql.apollo.api.Input
+import com.apollographql.apollo.coroutines.toDeferred
+import com.apollographql.apollo.exception.ApolloException
 import com.example.rocketreserver.databinding.LaunchListFragmentBinding
+import kotlinx.coroutines.channels.Channel
 
 class LaunchListFragment : Fragment() {
     private lateinit var binding: LaunchListFragmentBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         binding = LaunchListFragmentBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launchWhenResumed {
+            val response = try {
+                apolloClient.query(LaunchListQuery()).toDeferred().await()
+            } catch (e: ApolloException) {
+                Log.d("LaunchList", "Failure", e)
+                null
+            }
+
+            Log.d("LaunchList", "Success ${response?.data}")
+        }
     }
 }
