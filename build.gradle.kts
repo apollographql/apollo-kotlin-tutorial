9cdecddf85b621d281fb53fbe0b0ac22ef6f4416
Martin Bonnin <martin@mbonnin.net>
2020-11-09 15:29:13 +0100

    bump AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.0")
+        classpath("com.android.tools.build:gradle:4.0.2")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
     }
