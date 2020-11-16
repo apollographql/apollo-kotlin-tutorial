540a025f34539bffe341a721ec3717b46bf7f2a3
Martin Bonnin <martin@mbonnin.net>
2020-11-16 20:29:36 +0100

    update Gradle and AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,16 @@
 buildscript {
     repositories {
         google()
-        jcenter()
+        mavenCentral()
+        jcenter {
+            content {
+                includeGroup("org.jetbrains.trove4j")
+            }
+        }
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.2")
+        classpath("com.android.tools.build:gradle:4.1.1")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.2.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
     }
@@ -14,7 +19,7 @@ buildscript {
 allprojects {
     repositories {
         google()
-        jcenter()
+        mavenCentral()
     }
 }
 
