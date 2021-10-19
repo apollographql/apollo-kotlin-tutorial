ffdc7386cb7a0a66b8184c63c4fe9c8b080f7499
BoD <BoD@JRAF.org>
2021-10-19 09:41:04 +0200

    Update dependencies to latest versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,13 +5,12 @@ plugins {
 }
 
 android {
-    compileSdkVersion(29)
-    buildToolsVersion("29.0.3")
+    compileSdkVersion(31)
 
     defaultConfig {
         applicationId = "com.example.rocketreserver"
         minSdkVersion(23)
-        targetSdkVersion(29)
+        targetSdkVersion(31)
         versionCode = 1
         versionName = "1.0"
     }
@@ -33,19 +32,19 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.1.0")
-    implementation("androidx.core:core-ktx:1.3.0")
-    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.2.0")
-    implementation("androidx.recyclerview:recyclerview:1.1.0")
-    implementation("io.coil-kt:coil:0.11.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.3.0-rc01")
-    implementation("androidx.navigation:navigation-ui-ktx:2.3.0-rc01")
-    implementation("androidx.paging:paging-runtime-ktx:2.1.2")
-    implementation("com.google.android.material:material:1.1.0")
-    implementation("androidx.security:security-crypto:1.0.0-rc02")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("io.coil-kt:coil:1.4.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+    implementation("androidx.paging:paging-runtime-ktx:3.0.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.security:security-crypto:1.1.0-alpha03")
 
-    testImplementation("junit:junit:4.13")
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
