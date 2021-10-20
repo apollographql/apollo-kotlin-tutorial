19cbb143904fad094374b01f31cffc7f06062794
BoD <BoD@JRAF.org>
2021-10-20 11:00:39 +0200

    Use OkHttp interceptor instead of Apollo interceptor

--- app/src/main/java/com/example/rocketreserver/Apollo.kt
+++ app/src/main/java/com/example/rocketreserver/Apollo.kt
@@ -2,14 +2,12 @@ package com.example.rocketreserver
 
 import android.content.Context
 import com.apollographql.apollo3.ApolloClient
-import com.apollographql.apollo3.api.ApolloRequest
-import com.apollographql.apollo3.api.ApolloResponse
-import com.apollographql.apollo3.api.Operation
-import com.apollographql.apollo3.api.http.httpHeader
-import com.apollographql.apollo3.interceptor.ApolloInterceptor
-import com.apollographql.apollo3.interceptor.ApolloInterceptorChain
+import com.apollographql.apollo3.network.http.HttpNetworkTransport
+import com.apollographql.apollo3.network.ws.DefaultWebSocketEngine
 import com.apollographql.apollo3.network.ws.WebSocketNetworkTransport
-import kotlinx.coroutines.flow.Flow
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 
 private var instance: ApolloClient? = null
 
@@ -18,23 +16,34 @@ fun apolloClient(context: Context): ApolloClient {
         return instance!!
     }
 
-    instance = ApolloClient.Builder()
-        .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com/graphql")
-        .subscriptionNetworkTransport(WebSocketNetworkTransport("wss://apollo-fullstack-tutorial.herokuapp.com/graphql"))
+    val okHttpClient = OkHttpClient.Builder()
         .addInterceptor(AuthorizationInterceptor(context))
+        .build()
+
+    instance = ApolloClient.Builder()
+        .networkTransport(
+            HttpNetworkTransport(
+                serverUrl = "https://apollo-fullstack-tutorial.herokuapp.com/graphql",
+                okHttpClient = okHttpClient
+            )
+        )
+        .subscriptionNetworkTransport(
+            WebSocketNetworkTransport(
+                serverUrl = "wss://apollo-fullstack-tutorial.herokuapp.com/graphql",
+                webSocketEngine = DefaultWebSocketEngine(okHttpClient)
+            )
+        )
         .build()
 
     return instance!!
 }
 
-private class AuthorizationInterceptor(val context: Context) : ApolloInterceptor {
-    override fun <D : Operation.Data> intercept(
-        request: ApolloRequest<D>,
-        chain: ApolloInterceptorChain
-    ): Flow<ApolloResponse<D>> {
-        val requestWithHeader = request.newBuilder()
-            .httpHeader("Authorization", User.getToken(context) ?: "")
+private class AuthorizationInterceptor(val context: Context) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request().newBuilder()
+            .addHeader("Authorization", User.getToken(context) ?: "")
             .build()
-        return chain.proceed(requestWithHeader)
+
+        return chain.proceed(request)
     }
 }
