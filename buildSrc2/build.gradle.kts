9afab08f023ea08abc0e31d6b8505a6b1bc3fffe
Martin Bonnin <martin@mbonnin.net>
2022-06-22 16:10:55 +0200

    bump tutorial to 3.3.2

---
+++ buildSrc2/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm").version("1.5.31")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.apollographql.apollo3:apollo-gradle-plugin:3.3.2")
+}
