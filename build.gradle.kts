4ed5d840f0d5e8e3e31e18b87a10e386dffc9379
Shane Myrick <shane@apollographql.com>
2022-09-27 10:54:26 -0700

    Update Android Gradle Plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.2")
+        classpath("com.android.tools.build:gradle:7.3.0")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.4.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
     }
