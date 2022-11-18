cd3c77bdd1fc0634576e68f724e8d7948abd8485
Martin Bonnin <martin@mbonnin.net>
2022-11-18 12:24:24 +0100

    update Apollo to 3.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("androidx.navigation.safeargs.kotlin")
-    id("com.apollographql.apollo3").version("3.7.0")
+    id("com.apollographql.apollo3").version("3.7.1")
 }
 
 android {
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        maven("https://s01.oss.sonatype.org/content/repositories/comapollographql-1045/")
     }
 
     dependencies {
@@ -15,6 +16,7 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        maven("https://s01.oss.sonatype.org/content/repositories/comapollographql-1045/")
     }
 }
 
