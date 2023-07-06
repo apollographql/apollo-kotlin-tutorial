bb1c430203dbfa26d2e3a3e86370ebe4e13f1996
Martin Bonnin <martin@mbonnin.net>
2023-07-06 12:51:01 +0200

    actually use the test builders

--- compose/final/app/src/test/java/com/example/rocketreserver/ExampleUnitTest.kt
+++ compose/final/app/src/test/java/com/example/rocketreserver/ExampleUnitTest.kt
@@ -1,5 +1,7 @@
 package com.example.rocketreserver
 
+import com.example.rocketreserver.test.BookTripMutation_TestBuilder
+import com.example.rocketreserver.test.BookTripMutation_TestBuilder.Data
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +15,11 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+        val data = BookTripMutation.Data {
+            bookTrips = bookTrips {
+                message = ""
+            }
+        }
+        println(data)
     }
 }
