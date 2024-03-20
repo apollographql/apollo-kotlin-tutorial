9c59567b13e25305e9dadf9ca7f313ade77f2ecd
Benoit 'BoD' Lubek <BoD@JRAF.org>
2024-03-20 18:55:40 +0100

    Bump to 3.8.3

--- compose/final/app/build.gradle.kts
+++ compose/final/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.apollographql.apollo3").version("3.8.2")
+    id("com.apollographql.apollo3").version("3.8.3")
 }
 
 android {
